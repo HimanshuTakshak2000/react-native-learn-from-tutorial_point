042a54c8a59dcdb4cf143f52a7a3f0787dfe2492
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-05 16:27:11 +0530

    React Native State by Tutorial point

--- App.tsx
+++ App.tsx
@@ -1,23 +1,83 @@
-import React, { Component } from 'react'  
-import {StyleSheet,View, Text} from 'react-native'  
+// Example - 1
+// import React, {Component} from 'react';  
+// import { Text, View } from 'react-native';  
   
-export default class SwitchExample extends Component {  
+// export default class App extends Component {  
+//     state = {  // state --> must be written like this only;
+//         myState: 'This is a text component, created using state data. It will change or updated on clicking it.'  , // this is how we intially define state in class component 
+//         secondState:" This is how we define state in class component",
+//     }  
+
+//     updateState = () => { // this is how we will create a function class component
+
+//       if(this.state.myState === 'This is a text component, created using state data. It will change or updated on clicking it.'){
+//         this.setState({myState: 'The state is updated'});
+//       }
+//       else{
+//         this.setState({myState:'This is a text component, created using state data. It will change or updated on clicking it.'});
+//       }
+//     }
+
+//     render() {  
+//         return (  
+//             <View>  
+//                 <Text onPress={this.updateState} style={{fontSize:20, textAlign:'center', padding:10, borderColor:'pink', borderWidth:4, margin:15, height:200, verticalAlign:'middle'}}> {this.state.myState} </Text>  
+//             </View>  
+//         );  
+//     }  
+// }  
+
+
+// Example - 2 --> interchange the Text value "Show" and "Hide" to show and hide the input password
+
+import React, { Component } from 'react';  
+import {StyleSheet,Text,View,TextInput,TouchableOpacity} from 'react-native';  
+  
+export default class App extends Component {  
+    state = {  
+        password: '',  
+        isPasswordVisible: true,  
+        toggleText: 'Show',  
+    }  
+
+    // constructor(props: Props) {  
+    //     super(props);  
+    //     this.state = {  
+    //         password: '',  
+    //         isPasswordVisible: true,  
+    //         toggleText: 'Show',  
+    //     };  
+    // }  
+
+    handleToggle = () => {  
+        const { isPasswordVisible } = this.state;  
+        if (isPasswordVisible) {  
+            this.setState({ isPasswordVisible: false });  
+            this.setState({ toggleText: 'Hide' });  
+        } else {  
+            this.setState({ isPasswordVisible: true });  
+            this.setState({ toggleText: 'Show' });  
+        }  
+    };  
     render() {  
         return (  
             <View style={styles.container}>  
-                <View style={{backgroundColor: 'blue',flex:0.2, width:'50%'}} />  
-                <View style={{backgroundColor: 'red', flex:0.5}} />  
-                <Text style={{fontSize: 18, backgroundColor:"purple", height:100,}}>View Example</Text>  
+                <TextInput  
+                    secureTextEntry={this.state.isPasswordVisible}  
+                    style={{width:'100%', height: 50, backgroundColor: '#a7a6a9', color: 'white', fontSize: 20, marginBottom:10 }}  
+                />  
+                <TouchableOpacity onPress={this.handleToggle}>  
+                    <Text  style={{fontSize: 20}}>{this.state.toggleText}</Text>  
+                </TouchableOpacity>  
             </View>  
         );  
     }  
 }  
-const styles = StyleSheet.create ({  
-     container: {  
-         flex: 1,  
-        //  flexDirection: 'column',  // no need to add column as it is by-default flex direction
-        // height : 200, // no need to add height if we have provided flex property to the component 
-         width: "80%",  
-         backgroundColor:"green" // #5ead97  
-     }  
-})  
+const styles = StyleSheet.create({  
+    container: {  
+        flex: 1,  
+        justifyContent: 'center',  
+        alignItems: 'center',  
+        padding:15,
+    }  
+});  
