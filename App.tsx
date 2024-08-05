da4f692b84e5a62732bddbe207753836758aedad
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-05 17:03:00 +0530

    React Native Props by Tutorial Point

--- App.tsx
+++ App.tsx
@@ -1,83 +1,67 @@
-// Example - 1
-// import React, {Component} from 'react';  
-// import { Text, View } from 'react-native';  
+// // Example - 1
+// import React, { Component } from 'react';  
+// import {   
+//   StyleSheet,  
+//   Image,  
+//   Text,  
+//   View  
+// } from 'react-native';  
   
-// export default class App extends Component {  
-//     state = {  // state --> must be written like this only;
-//         myState: 'This is a text component, created using state data. It will change or updated on clicking it.'  , // this is how we intially define state in class component 
-//         secondState:" This is how we define state in class component",
-//     }  
-
-//     updateState = () => { // this is how we will create a function class component
-
-//       if(this.state.myState === 'This is a text component, created using state data. It will change or updated on clicking it.'){
-//         this.setState({myState: 'The state is updated'});
-//       }
-//       else{
-//         this.setState({myState:'This is a text component, created using state data. It will change or updated on clicking it.'});
-//       }
-//     }
-
-//     render() {  
-//         return (  
-//             <View>  
-//                 <Text onPress={this.updateState} style={{fontSize:20, textAlign:'center', padding:10, borderColor:'pink', borderWidth:4, margin:15, height:200, verticalAlign:'middle'}}> {this.state.myState} </Text>  
-//             </View>  
-//         );  
-//     }  
+  
+// export default class App extends Component<{}> {  
+//   render() {  
+//     let imagePath = { uri: 'https://facebook.github.io/react-native/img/header_logo.png'};  
+//     return (  
+//         <View style={styles.container}>  
+//           <Text style={styles.welcome}>Welcome to React Native!</Text>  
+//           <Image source={imagePath} style={{width: 250, height: 250}} />  
+//         </View>  
+//     );  
+//   }  
 // }  
+  
+// const styles = StyleSheet.create({  
+//   container: {  
+//     flex: 1,  
+//     justifyContent: 'center',  
+//     alignItems: 'center',  
+//     backgroundColor: '#a7a6a9',  
+//   },  
+//   welcome: {  
+//     fontSize: 30,  
+//     textAlign: 'center',  
+//     margin: 20,  
+//   }  
+// });  
 
-
-// Example - 2 --> interchange the Text value "Show" and "Hide" to show and hide the input password
+// Example - 2 -->
 
 import React, { Component } from 'react';  
-import {StyleSheet,Text,View,TextInput,TouchableOpacity} from 'react-native';  
+import { StyleSheet, Text, View } from 'react-native';  
   
-export default class App extends Component {  
-    state = {  
-        password: '',  
-        isPasswordVisible: true,  
-        toggleText: 'Show',  
-    }  
-
-    // constructor(props: Props) {  
-    //     super(props);  
-    //     this.state = {  
-    //         password: '',  
-    //         isPasswordVisible: true,  
-    //         toggleText: 'Show',  
-    //     };  
-    // }  
-
-    handleToggle = () => {  
-        const { isPasswordVisible } = this.state;  
-        if (isPasswordVisible) {  
-            this.setState({ isPasswordVisible: false });  
-            this.setState({ toggleText: 'Hide' });  
-        } else {  
-            this.setState({ isPasswordVisible: true });  
-            this.setState({ toggleText: 'Show' });  
-        }  
-    };  
-    render() {  
-        return (  
-            <View style={styles.container}>  
-                <TextInput  
-                    secureTextEntry={this.state.isPasswordVisible}  
-                    style={{width:'100%', height: 50, backgroundColor: '#a7a6a9', color: 'white', fontSize: 20, marginBottom:10 }}  
-                />  
-                <TouchableOpacity onPress={this.handleToggle}>  
-                    <Text  style={{fontSize: 20}}>{this.state.toggleText}</Text>  
-                </TouchableOpacity>  
-            </View>  
-        );  
-    }  
+class ChildClass extends Component<{name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined}> {   // we must send props name in components type
+  render() {  
+    return (  
+        <View style={{alignItems: 'center'}}>  
+          <Text style={styles.welcome}>Hello {this.props.name}!</Text>  
+        </View>  
+    );  
+  }  
+}  
+  
+export default class ParentsClass extends Component {  
+  render() {  
+    return (  
+        <View style={{alignItems: 'center'}}>  
+          <ChildClass name='Ashu' />  
+          <ChildClass name='Aman' />  
+          <ChildClass name='Max' />  
+        </View>  
+    );  
+  }  
 }  
 const styles = StyleSheet.create({  
-    container: {  
-        flex: 1,  
-        justifyContent: 'center',  
-        alignItems: 'center',  
-        padding:15,
-    }  
+   welcome: {  
+    fontSize: 30,  
+  }  
 });  
