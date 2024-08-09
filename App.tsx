0d99d7a8bae3076d5d5e2b76fe493f9a47c4d135
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-09 17:27:58 +0530

    React Native Button from tutorial point

--- App.tsx
+++ App.tsx
@@ -1,70 +1,65 @@
+
 // Example - 1
-// // Fixed Dimensions - Using fixed height and fixed width in style is the simplest way to set the dimension of the component. The dimensions of React Native component are unit-less and they represent density-independent pixels.Setting the dimension of component with fixed size is common and exactly the same size, regardless of screen dimensions.
-
-// import React, { Component } from 'react';  
-// import { StyleSheet, View } from 'react-native';  
-  
-// export default class HeightWidth extends Component {  
-//     render() {  
-//         return (  
-//             <View style={{margin:10, backgroundColor:'white'}}>  
-//                 <View style={styles.powderblue} />  
-//                 <View style={styles.skyblue} />  
-//                 <View style={styles.steelblue} />  
-//             </View>  
-//         );  
-//     }  
-// }  
-// const styles = StyleSheet.create({  
-//     powderblue:{  
-//         width: 100, height: 100, backgroundColor: 'powderblue'  
-//     },  
-//     skyblue:{  
-//         width: 200, height: 200, backgroundColor: 'skyblue'  
-//     },  
-//     steelblue:{  
-//         height: 300, backgroundColor: 'steelblue'  
-//     },  
-// })  
-
-
-// Example - 2 
-// Flex Dimensions - The flex property styles the component to expand and shrink it dynamically according to available space. Setting flex: 1 will fill all the available space to the component and shared evenly among the other components of same as the parent. Higher the flex value, occupy component higher ratio of space compared to its siblings.
+// Button - React Native Button is a basic component that works by clicking on it. It imports the Button class of react-native.
 
 import React, { Component } from 'react';  
-import { StyleSheet, View } from 'react-native';  
+import { Alert, Button, StyleSheet, View } from 'react-native';  
   
-export default class HeightWidth extends Component {  
+export default class ButtonBasics extends Component {  
+    onPressButton() {  
+        Alert.alert('You clicked the button!')  
+    }  
+  
     render() {  
         return (  
             <View style={styles.container}>  
-                <View style={styles.powderblue} />  
-                <View style={styles.skyblue} />  
-                <View style={styles.steelblue} />  
+
+                <View style={styles.buttonContainer}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="Press Me"  
+                    />  
+                </View>  
+
+                <View style={styles.buttonContainer}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="Press Me"  
+                        color="#009933"  
+                    />  
+                </View>  
+                
+                <View style={styles.multiButtonContainer}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="A disabled button"  
+                        disabled={true}  
+                    />  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="OK!"  
+                        color="#009933"  
+                    />  
+                </View>  
             </View>  
         );  
     }  
 }  
+  
 const styles = StyleSheet.create({  
-    container:{  
-      flex:1,
-      margin:15,
-      borderColor:'red',
-      borderWidth:2,  
-    },  
-    powderblue:{  
-        flex:1,  
-        backgroundColor: 'powderblue',  
-    },  
-    skyblue:{  
-        flex:2,  
-        backgroundColor: 'skyblue',  
-    },  
-    steelblue:{  
-        flex:3,  
-        backgroundColor: 'steelblue',  
-    },  
-}) 
 
+    container: {  
+        flex: 1,  
+        justifyContent: 'center',  
+    },  
 
-
+    buttonContainer: {  
+        margin: 20  
+    },  
+    
+    multiButtonContainer: {  
+        margin: 20,  
+        flexDirection: 'row',  
+        justifyContent: 'space-between'  
+    }  
+})  
