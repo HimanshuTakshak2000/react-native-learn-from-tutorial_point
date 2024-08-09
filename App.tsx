bc3a0e717b9422f54b2fbdc27b42c4a2d3b43bf6
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-09 19:22:09 +0530

    React Native Layout and Flexbox from tutorial point

--- App.tsx
+++ App.tsx
@@ -1,65 +1,147 @@
+// Flexbox --> React Native Flexbox is an algorithm to specify the layout of component's children. It provides a consistent layout on different screen sizes. Flexbox provides three main properties to achieve the desired layout. These properties are: flexDirection, justifyContent and alignItems.
 
-// Example - 1
-// Button - React Native Button is a basic component that works by clicking on it. It imports the Button class of react-native.
+// flexDirection - The flexDirection adds the style to the component in a primary axis of its layout. It has a property row and column to organize children horizontally and vertically respectively. The default flexDirection is a column.
+
+// // Example - 1
+
+// import React, { Component } from 'react';  
+// import { StyleSheet,View } from 'react-native';  
+  
+// export default class FlexDirectionBasics extends Component {  
+//     render() {  
+//         return (  
+//             <View style={styles.container}>  
+//                 <View style={styles.powderblue} />  
+//                 <View style={styles.skyblue} />  
+//                 <View style={styles.steelblue} />  
+//             </View>  
+//         );  
+//     }  
+// }  
+// const styles = StyleSheet.create({  
+//     container:{  
+//         flex: 1,  
+//         flexDirection: 'row',// set elements horizontally, try column.  
+//     },
+
+//     powderblue:{  
+//         width: 60,  
+//         height: 60,  
+//         backgroundColor: 'powderblue',  
+//     },  
+
+//     skyblue:{  
+//         width: 60,  
+//         height: 60,  
+//         backgroundColor: 'skyblue',  
+//     }, 
+
+//     steelblue:{  
+//         width: 60,  
+//         height: 60,  
+//         backgroundColor: 'steelblue',  
+//     }  
+// })  
+
+// ============================================================== //
+
+// justifyContent - The justifyContent determines the distribution of children component along the primary axis. The children component are distributed at the start, end, center or space evenly.
+
+// // Example - 2
+
+// import React, { Component } from 'react';  
+// import { StyleSheet,View } from 'react-native';  
+  
+// export default class JustifyContentBasics extends Component {  
+//     render() {  
+//         return (  
+//             <View style={styles.container}>  
+//                 <View style={styles.powderblue} />  
+//                 <View style={styles.skyblue} />  
+//                 <View style={styles.steelblue} />  
+//             </View>  
+//         );  
+//     }  
+// }  
+// const styles = StyleSheet.create({  
+//     container:{  
+//         flex: 1,  
+//         flexDirection: 'column', // set elements horizontally`.  
+//         justifyContent: 'center',  
+//     },  
+
+//     powderblue:{  
+//         width: 60,  
+//         height: 60,  
+//         backgroundColor: 'powderblue'  
+//     }, 
+
+//     skyblue:{  
+//         width: 60,  
+//         height: 60,  
+//         backgroundColor: 'skyblue',  
+//     }, 
+
+//     steelblue:{  
+//         width: 60,  
+//         height: 60,  
+//         backgroundColor: 'steelblue',  
+//     }  
+// })  
+
+// ============================================================== //
+
+// alignItems - The alignItems determine the alignment of children component along the secondary axis. If the primary axis is a column then the secondary is a row and when a primary axis is a ro, then the secondary is a column. Using the alignItems, the children are aligned at start, end, center or stretched.
+// Note: The stretch does not work if the children have a fixed dimension along the secondary axis. In above example, the alignItems: stretch will not work until we remove width: 50.
+
+// Example - 3
 
 import React, { Component } from 'react';  
-import { Alert, Button, StyleSheet, View } from 'react-native';  
+import { StyleSheet,View } from 'react-native';  
   
-export default class ButtonBasics extends Component {  
-    onPressButton() {  
-        Alert.alert('You clicked the button!')  
-    }  
-  
+export default class AlignItemsBasics extends Component {  
     render() {  
         return (  
             <View style={styles.container}>  
-
-                <View style={styles.buttonContainer}>  
-                    <Button  
-                        onPress={this.onPressButton}  
-                        title="Press Me"  
-                    />  
-                </View>  
-
-                <View style={styles.buttonContainer}>  
-                    <Button  
-                        onPress={this.onPressButton}  
-                        title="Press Me"  
-                        color="#009933"  
-                    />  
-                </View>  
-                
-                <View style={styles.multiButtonContainer}>  
-                    <Button  
-                        onPress={this.onPressButton}  
-                        title="A disabled button"  
-                        disabled={true}  
-                    />  
-                    <Button  
-                        onPress={this.onPressButton}  
-                        title="OK!"  
-                        color="#009933"  
-                    />  
-                </View>  
+                <View style={styles.powderblue} />  
+                <View style={styles.skyblue} />  
+                <View style={styles.steelblue} />  
             </View>  
         );  
     }  
 }  
-  
 const styles = StyleSheet.create({  
-
-    container: {  
+    container:{  
         flex: 1,  
+        flexDirection: 'column', // set elements horizontally`.  
         justifyContent: 'center',  
+        alignItems: 'stretch',  
     },  
 
-    buttonContainer: {  
-        margin: 20  
+    powderblue:{  
+        width: 60,  
+        height: 60,  
+        backgroundColor: 'powderblue'  
+    }, 
+
+    skyblue:{  
+        width: 60,  
+        height: 60,  
+        backgroundColor: 'skyblue',  
     },  
-    
-    multiButtonContainer: {  
-        margin: 20,  
-        flexDirection: 'row',  
-        justifyContent: 'space-between'  
+
+    steelblue:{  
+        // width: 60, 
+        height: 60,  
+        backgroundColor: 'steelblue',  
     }  
 })  
+
+// ============================================================== //
+
+
+
+
+
+
+
