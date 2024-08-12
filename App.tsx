1021e6714130a49bee832cfe2df891ff8eb6a4f7
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-12 22:44:36 +0530

    React Native SectionList from tutorial point

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-// FlatList --> The FlatList component displays the similar structured data in a scrollable list. It works well for large lists of data where the number of list items might change over time. The FlatList shows only those renders elements which are currently displaying on the screen, not all the elements of the list at once. The FlatList component takes two required props: data and renderItem. The data is the source of elements for the list and renderItem takes one item from the source and returns a formatted component to render. To implement the FlatList component. We need to import FlatList from 'react-native' library.
+// SectionList --> The React Native SectionList component is a list view component which sets the list of data into broken logical section. The broken data can be implemented using its section header prop renderSectionHeader. To implement the SectionList component, we need to import SectionList from 'react-native' library. The sections prop is used to create the list of title and data values. The renderSectionHeader displays the header section of the list view.
+
+// ItemSeparatorComponent prop adds the Separator between the lists of data. Using this prop, call a renderSeparatormethod in which we add a View component as separator.
+
+// To perform an action on clicking (pressing) the list item, we use a onPress prop. The onPress prop and handle the event in another method getListViewItem.
 
 
 // Example - 1
 import React, { Component } from 'react';  
-import { FlatList, StyleSheet, Text, View,Alert } from 'react-native';  
+import { SectionList, StyleSheet, Text, View, Alert } from 'react-native';  
   
-export default class App extends Component {  
-  
+export default class SectionListBasics extends Component {  
+
     renderSeparator = () => {  
         return (  
             <View  
@@ -18,80 +22,37 @@ export default class App extends Component {
             />  
         );  
     };  
-
+    
     //handling onPress action  
-    getListViewItem = (item: { key: string; }) => {  
-        Alert.alert(item.key);  
+    getListViewItem = (item: any) => {  
+        Alert.alert(item);  
     }  
-  
+
+
     render() {  
         return (  
             <View style={styles.container}>  
-                <FlatList 
-
-                    data={[  
+                <SectionList  
+                    sections={[  
                         {
-                            key: 'Android'
-                        },
-
-                        {
-                            key: 'iOS'
-                        },
-
-                        {
-                            key: 'Java'
-                        },
-
-                        {
-                            key: 'Swift'
-                        },
-
-                        {
-                            key: 'Php'
-                        },
-
-                        {
-                            key: 'Hadoop'
-                        },
-
-                        {
-                            key: 'Sap'
+                            title: 'A', 
+                            data: ['ALTERED','ABBY','ACTION U.S.A.','AMUCK','ANGUISH']
                         }, 
 
                         {
-                            key: 'Python'
-                        },
-
-                        {
-                            key: 'Ajax'
+                            title: 'B', 
+                            data: ['BEST MEN','BEYOND JUSTICE','BLACK GUNN','BLOOD RANCH','BEASTIES']
                         }, 
-                        
+                         
                         {
-                            key: 'C++'
-                        },
-
-                        {
-                            key: 'Ruby'
-                        },
-
-                        {
-                            key: 'Rails'
-                        },
-
-                        {
-                            key: '.Net'
-                        },
-                          
-                        {
-                            key: 'Perl'
-                        } 
+                            title: 'C', 
+                            data: ['CARTEL', 'CASTLE OF EVIL', 'CHANCE', 'COP GAME', 'CROSS FIRE',]
+                        },  
                     ]}  
-
-                    renderItem={({item}) =>  
-                        <Text style={styles.item} onPress={this.getListViewItem.bind(this, item)}> {item.key} </Text>
-                    }  
-
+                    renderItem={({item}) => <Text style={styles.item} onPress={this.getListViewItem.bind(this, item)} >{item}</Text>}  
+                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}  
                     ItemSeparatorComponent={this.renderSeparator}  
+
                 />  
             </View>  
         );  
@@ -101,31 +62,27 @@ export default class App extends Component {
 const styles = StyleSheet.create({  
     container: {  
         flex: 1,  
+        backgroundColor: "#5ead97"  
+    },  
+
+    sectionHeader: {  
+        paddingTop: 2,  
+        paddingLeft: 10,  
+        paddingRight: 10,  
+        paddingBottom: 2,  
+        fontSize: 22,  
+        fontWeight: 'bold',  
+        color: "#fff",  
+        backgroundColor: '#8fb1aa',  
     }, 
 
     item: {  
         padding: 10,  
-        fontSize: 24,  
-        height: 80,  
-    },  
+        fontSize: 18,  
+        height: 50,  
+    }  
 })  
 
-
-
-// ============================================================== //
-
-
-// Example - 2
-
-
-// ============================================================== //
-
-
-// Example - 3
-
-
-
-// ============================================================== //
 
 
 
