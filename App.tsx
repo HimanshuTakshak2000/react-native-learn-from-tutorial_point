e688520672076b3e61b87dee016b78186d7b47be
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-12 13:08:42 +0530

    React Native View from tutorial pointScrollView

--- App.tsx
+++ App.tsx
@@ -1,167 +1,147 @@
-// Flexbox Positioning -->
+// ScrollView --> The ScrollView is a generic scrollable container, which scrolls multiple child components and views inside it. In the ScrollView, we can scroll the components in both direction vertically and horizontally. By default, the ScrollView container scrolls its components and views in vertical. To scroll its components in horizontal, it uses the props horizontal: true (default, horizontal: false).
+
 
 // // Example - 1
-// import React, { Component } from "react";  
-// import { StyleSheet, TextInput, View , Button, Alert } from "react-native";  
-  
-// export default class App extends Component {  
+// // Vertical Scroll View
 
-//     state = {  
-//         placeName: "",  
-//     };  
+// import React, { Component } from 'react';  
+// import { ScrollView, Text, Button, Alert } from 'react-native';  
   
-//     placeNameChangedHandler = (val: any) => {  
-//         this.setState({  
-//             placeName: val  
-//         });  
-//     };  
-  
-//     placeSubmitHandler = () => {  
-//         Alert.alert("button clicked")  ;
-//     };  
+// export default class ScrolledViewExample extends Component {  
+//     onPressButton() {  
+//         Alert.alert('You clicked the button!')  
+//     }  
   
 //     render() {  
 //         return (  
-//             <View style={styles.container}>  
-//                 <TextInput  
-//                         placeholder="An awesome place"  
-//                         onChangeText={this.placeNameChangedHandler}  
-//                 />  
-//                 <Button  
-//                         title="Button"  
-//                         onPress={this.placeSubmitHandler}  
-//                 />  
-//             </View>  
+//             <ScrollView style={{padding:15}}>  
+//                 <Text style={{fontSize:20}}>Scroll me plz</Text>  
+//                 <Button title={'Button 1'} onPress={this.onPressButton} />  
+//                 <Text style={{fontSize:20}}>React Native Example of ScrollView</Text>  
+//                 <Button title={'Button 2'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>React Native ScrollView Example</Text>  
+//                 <Button title={'Button 3'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>If you like</Text>  
+//                 <Button title={'Button 4'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>Scrolling down</Text>  
+//                 <Button title={'Button 5'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>Scrolling down</Text>  
+//                 <Button title={'Button 6'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>What's the best</Text>  
+//                 <Button title={'Button 7'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>What's the best</Text>  
+//                 <Button title={'Button 8'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>Framework around?</Text>  
+//                 <Button title={'Button 9'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>Framework around?</Text>  
+//                 <Button title={'Button 10'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>React Native</Text>  
+//                 <Button title={'Button 11'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>Scroll me plz</Text>  
+//                 <Button title={'Button 12'} onPress={this.onPressButton} />  
+//                 <Text style={{fontSize:20}}>Scroll me plz</Text>  
+//                 <Button title={'Button 13'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>If you like</Text>  
+//                 <Button title={'Button 14'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>If you like</Text>  
+//                 <Button title={'Button 15'} onPress={this.onPressButton}/>  
+//                 <Text style={{fontSize:20}}>Scrolling down</Text>  
+//                 <Button title={'Button 16'} onPress={this.onPressButton}/>  
+//             </ScrollView>  
 //         );  
 //     }  
 // }  
-  
-// const styles = StyleSheet.create({  
-//     container: {  
-//         flex: 1,  
-//         padding: 26,  
-//         justifyContent: "flex-start"  
-//     }  
-// });  
+
 
 // ============================================================== //
 
 
-// // Example - 2
-// import React, { Component } from "react";  
-// import { StyleSheet, View, TextInput, Button, Alert } from "react-native";  
-  
-// export default class App extends Component {  
-//     state = {  
-//         placeName: "",  
-//     };  
-  
-//     placeNameChangedHandler = (val: any) => {  
-//         this.setState({  
-//             placeName: val  
-//         });  
-//     };  
-  
-//     placeSubmitHandler = () => {  
-//         Alert.alert("button clicked")  
-//     };  
-  
-//     render() {  
-//         return (  
-//             <View style={styles.container}>  
-//                 <View style={styles.innerContainer}>  
-//                     <TextInput  
-//                             placeholder="An awesome place"  
-//                             onChangeText={this.placeNameChangedHandler}  
-//                     />  
-//                     <Button  
-//                             title="Button"  
-//                             onPress={this.placeSubmitHandler}  
-//                     />  
-//                 </View>  
-//             </View>  
-//         );  
-//     }  
-// }  
-  
-// const styles = StyleSheet.create({  
-//     container: {  
-//         flex: 1,  
-//         padding: 26,  
-//         justifyContent: "flex-start"  
-//     },  
-    
-//     innerContainer:{  
-//         flex: 1,  
-//         flexDirection: "row"  
-//     }  
-// });  
+// Example - 2
+// Horizontal ScrollView - The horizontal ScrollView scrolls the components and views from left to right. It will be implemented by setting the props horizontal to true (horizontal={true}).
 
-// // Note :- The flex: 1 for inner View occupy full space which does not look good as the TextInput and Button occupy all space from top to bottom.
+import React, { Component } from 'react';  
+import { ScrollView, View, Text, Button, StyleSheet, Alert } from 'react-native';  
+  
+export default class ScrolledViewExample extends Component {  
+    onPressButton() {  
+        Alert.alert('You clicked the button!')  
+    }  
+  
+    render() {  
+        return (  
+            <ScrollView  horizontal={true} style={styles.container} contentContainerStyle={styles.scrollViewContainer}>  
+                <Text style={{fontSize:22, padding: 10}}>Horizontal ScrollView</Text>  
 
+                <View style={[{ width: 220,height: 70,padding: 10 }]}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="Button 1"  
+                        color="#FF3D00"  
+                    />  
+                </View>  
+
+                <Text style={{fontSize:22, padding: 10}}>javatpoint</Text>  
+                <View style={[{ width: 220,height: 70,padding: 10 }]}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="Button 2"  
+                        color="#3D00FF"  
+                    />  
+                </View>  
+                
+                <Text style={{fontSize:22, padding: 10}}>React Native ScrollView Example</Text>  
+                <View style={[{ width: 220,height: 70,padding: 10 }]}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="Button 3"  
+                        color="#FFFF3D"  
+                    />  
+                </View>  
+
+                <Text style={{fontSize:22, padding: 10}}>If you like</Text>  
+                <View style={[{ width: 220,height: 70,padding: 10 }]}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="Button 4"  
+                        color="#FF3DFF"  
+                    />  
+                </View>  
+
+                <Text style={{fontSize:22, padding: 10}}>Scrolling horizontal</Text>  
+                <View style={[{ width: 220,height: 70,padding: 10 }]}>  
+                    <Button  
+                        onPress={this.onPressButton}  
+                        title="Button 5"  
+                        color="#3DFF00"  
+                    />  
+                </View>  
+            </ScrollView>  
+        );  
+    }  
+}  
+
+const styles = StyleSheet.create({  
+    container:{  
+        flex: 1,  
+    }, 
+
+    scrollViewContainer:{
+        alignItems:'center'
+    },
+
+    buttonStyle:{ 
+        height: 50, 
+        width: 70, 
+    }
+})  
+
+// Note - If we want to align item and justifyContent then we have to use contentContainerStyle prop to write these propperty inside this prop.
 
 // ============================================================== //
 
 
 // Example - 3
 
-import React, { Component } from "react";  
-import { StyleSheet, View, TextInput, Button, Alert } from "react-native";  
-  
-export default class App extends Component {  
-    state = {  
-        placeName: "",    
-    };  
-  
-    placeNameChangedHandler = (val : any) => {  
-        this.setState({  
-            placeName: val  
-        });  
-    };  
-  
-    placeSubmitHandler = () => {  
-        Alert.alert("button clicked")  
-    };  
-  
-    render() {  
-        return (  
-            <View style={styles.container}>  
-                <View style={styles.innerContainer}>  
-                    <TextInput  
-                            placeholder="An awesome place"  
-                            onChangeText={this.placeNameChangedHandler}  
-                            style={styles.textStyle}  
-                    />  
-                    <Button  
-                            title="Button"  
-                            onPress={this.placeSubmitHandler}  
-                    />  
-                </View>  
-            </View>  
-        );  
-    }  
-}  
-  
-const styles = StyleSheet.create({  
-    container: {  
-        flex: 1,  
-        padding: 26,  
-        backgroundColor: "#fff",  
-        justifyContent: "flex-start"  
-    },  
-    innerContainer:{   
-        flexDirection: "row",  
-        justifyContent: "space-between",  
-        alignItems: "center"  
-    },  
-    textStyle:{  
-        width: "70%",  
-        backgroundColor: "gray",  
-    },  
-    buttonStyle:{  
-        width: "30%",  
-    }  
-});  
 
 
 // ============================================================== //
