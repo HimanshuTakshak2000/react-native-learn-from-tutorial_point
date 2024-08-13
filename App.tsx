3dd49fdd068ca62aba40daf79b6b8873a76524b1
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-13 15:27:16 +0530

    React Native Text Input by Tutorial Point

--- App.tsx
+++ App.tsx
@@ -1,96 +1,127 @@
-// Touchables --> Touchable components provide the capability to capture the tapping functionality. The touchables component can be implemented as an alternative of basic button if they are not look right for your app. Using these components you build your own button. Tapping on these components you can display the feedback. The touchables components do not provide any default styling so you will need to do your style for presenting nicely in the app.
+// Text Input --> TextInput is the fundamental component to input text. It has several props which configure the different features such as onChangeText that takes a function and call it whenever the text changed. The onSubmitEditing prop takes a function which is called when the text submitted. There are several things which can be performed with text input such as validating the text inside while user types.
 
-/*
-    Types of Touchable Components -
-    There are four touchable components provided by React Native. Selection of this component depends on the kind of feedback you want to provide:
-    > TouchableHighlight : The TouchableHighlight can be used where you would use a button or link on the web. The background of this component becomes dark on pressing it.
+// // Example - 1
+// import React, { Component } from 'react';
+// import { Text, TextInput, View } from 'react-native';
 
-    > TouchableNativeFeedback : The TouchableNativeFeedback makes a view to response properly on touch. This component works only for Android operating system. It uses native state drawable to display the touch feedback.
+// interface PizzaTranslatorState {
+//     text: string;
+// }
 
-    > TouchableOpacity : The TouchableOpacity wrapper is used to reduce the opacity of button. It allows background to be seen while the user press down. The opacity of button will be controlled by wrapping the children in an Animation.
+// export default class PizzaTranslator extends Component<{}, PizzaTranslatorState> {
+//     constructor(props: any) {
+//         super(props);
+//         this.state = {
+//             text: ''
+//         };
+//     }
+    
+//     // Note - If we want to define state in the constructor then we have to create an interface.
+//     // state={
+//     //     text:'',
+//     // }
 
-    > TouchableWithoutFeedback : The TouchableWithoutFeedback is used when the user wants to handle the tap functionality but doesn't want to display any feedback.
+//     render() {
+//         return (
+//             <View style={{ padding: 10, margin:10 }}>
+//                 <TextInput
+//                     style={{ height: 40, backgroundColor: 'pink', fontSize: 20, color: 'black' }}
+//                     placeholder="Type here to translate!"
+//                     placeholderTextColor={'black'}
+//                     onChangeText={(text) => this.setState({ text })}
+//                 />
 
-    Note : Some time user presses a view and holds it for the set of time. This long press is handled by the function using onLongPress props of any of the above "Touchable" components.
+//                 <Text style={{ padding: 30, fontSize: 50, margin:10 }}>
+//                     {this.state.text.split(' ').map((word)=> word && '🍕').join(' ')}
+//                 </Text>
+//             </View>
+//         );
+//     }
+// }
 
-*/
+// ========================================================================== //
 
-// Example - 1
+// Multiline TextInput --> The multiline props provide facility to input multiple lines of text. Some props of TextInput are only compatible with multiline. For example: multiline={true/false}. The property borderButtomColor will not work if multiline = false.
+
+// Example - 2 
 import React, { Component } from 'react';  
-import { Alert,Platform,StyleSheet,Text,TouchableHighlight,TouchableOpacity,  
-    TouchableNativeFeedback,TouchableWithoutFeedback, View } from 'react-native';  
+import { View, TextInput } from 'react-native';  
+
+interface UselessTextInputMultilineSate {
+    text:string;
+}
+
+interface UselessTextInputProps {
+    value: string;
+    onChangeText: (text: string) => void;
+    multiline : boolean ; 
+    numberOfLines : number ; 
+    style: object,
+
+}
+
   
-export default class Touchables extends Component {  
-    onPressButton() {  
-        Alert.alert('You tapped the button!')  
-    }  
-  
-    onLongPressButton() {  
-        Alert.alert('You long-pressed the button!')  
-    }  
-  
-  
+class UselessTextInput extends Component<UselessTextInputProps>  {  
     render() {  
         return (  
-            <View style={styles.container}>  
+            <TextInput 
+            {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+            editable={true}
+            // maxLength={40} // --> due to this we are able to add only 40 letter i.e length of array is 40
+            value={this.props.value} // Use the passed value prop
+            onChangeText={this.props.onChangeText} // Handle text change
 
-                <TouchableHighlight onPress={this.onPressButton} underlayColor="pink">  
-                    <View style={styles.button}>  
-                        <Text style={styles.buttonText}>TouchableHighlight</Text>  
-                    </View>  
-                </TouchableHighlight>  
+            />  
+        );  
+    }  
+}  
 
-                <TouchableOpacity onPress={this.onPressButton} activeOpacity={.2}>  
-                    <View style={styles.button}>  
-                        <Text style={styles.buttonText}>TouchableOpacity</Text>  
-                    </View>  
-                </TouchableOpacity>  
+export default class UselessTextInputMultiline extends Component<{}, UselessTextInputMultilineSate> {  
 
-                <TouchableNativeFeedback  
-                    onPress={this.onPressButton} background={TouchableNativeFeedback.Ripple('rgba(255, 0, 0, 0.3)', true)} >  
-                    <View style={styles.button}>  
-                        <Text style={styles.buttonText}>TouchableNativeFeedback</Text>  
-                    </View>  
-                </TouchableNativeFeedback>  
+    constructor(props: any) {  
+        super(props);  
+        this.state = {  
+            text: 'Useless Multiline Placeholder',  
+        };  
+    }  
 
-                <TouchableWithoutFeedback  
-                    onPress={this.onPressButton}  
-                >  
-                    <View style={styles.button}>  
-                        <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>  
-                    </View>  
-                </TouchableWithoutFeedback> 
+    // state = {
+    //             text : 'Useless Multiline Placeholder',
+    //         } 
+  
+      
+    render() {  
+        return (  
+            <View style={{  
+                backgroundColor: this.state.text,  
+                borderBottomColor: 'white',  
+                borderBottomWidth: 1,  
+               }}  
+            >  
+                <UselessTextInput  
+                multiline = {true}  
+                numberOfLines = {10}  
+                onChangeText={(text) => this.setState({text})}  
+                value={this.state.text}  
+                style={{fontSize: 20, backgroundColor:'gray'}}  
+                />  
 
-                <TouchableHighlight onPress={this.onPressButton} onLongPress={this.onLongPressButton} underlayColor="white">  
-                    <View style={styles.button}>  
-                        <Text style={styles.buttonText}>Touchable with Long Press</Text>  
-                    </View>  
-                </TouchableHighlight>  
+                <View style={{margin:20}}></View>
+
+                <TextInput  
+                    multiline = {true}  
+                    numberOfLines = {10}  
+                    onChangeText={(text) => this.setState({text})}  
+                    value={this.state.text}  
+                    style={{fontSize: 20, backgroundColor:'gray'}}  
+                    editable={true}
+                    maxLength={40} // --> due to this we are able to add only 40 letter i.e length of array is 40
+                />  
 
             </View>  
         );  
     }  
 }  
-  
-const styles = StyleSheet.create({  
-    container: {  
-        paddingTop: 60,  
-        alignItems: 'center'  
-    }, 
-
-    button: {  
-        marginBottom: 30,  
-        width: 270,  
-        alignItems: 'center',  
-        backgroundColor: '#5ead97'  
-    }, 
-
-    buttonText: {  
-        padding: 20,  
-        color: 'white',  
-        fontSize: 18  
-    }  
-});  
 
 
 
